5f952a7d81c5cfb1c6f1c762005da7ccb0a2bd79
EduardoLott <dudulott@gmail.com>
2022-12-14 15:54:37 -0300

    Adicionar procura no banco de dados

--- src/services/observers/camsObservers.ts
+++ src/services/observers/camsObservers.ts
@@ -1,12 +1,14 @@
+import React from "react";
 import { db } from "../../firebaseconfig";
 
 const camsReference = db.collection('cameras');
 
-export function getCamsObserver(callback: any){
-    camsReference.onSnapshot((snapshot)=>{
+export function getCamsObserver(callback: React.Dispatch<React.SetStateAction<any[]>>, camsNomeCamera: string[]){
+    camsReference.where('NomeCamera','in',camsNomeCamera).onSnapshot((snapshot)=>{
         const cams: any[] = []
         snapshot.forEach((doc)=>{
             cams.push(doc.data());
         })
+        callback(cams);
     })
 }
